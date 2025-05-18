c51f1ffc7e2a5e310b9dd700d38757431be159d0
Ar-jay Dayanan <acdayanan.bposeats@gmail.com>
2025-05-18 15:42:05 +0800

    use django and django ninja for BE

--- be/db/schema/product.py
+++
@@ -1,5 +0,0 @@
-from db.models.product import *
-from tortoise.contrib.pydantic import pydantic_model_creator
-
-Product_Pydantic = pydantic_model_creator(Product, name="Product")
-ProductIn_Pydantic = pydantic_model_creator(Product, name="ProductIn", exclude_readonly=True)
