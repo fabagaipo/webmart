ee901513aed7dfbf8b745a52cabed1fedd173aef
Ar-jay Dayanan <acdayanan.bposeats@gmail.com>
2025-05-20 00:47:05 +0800

    Modify a few endpoints for product

--- webmart_api/webmart_api/product/product.py
+++ webmart_api/webmart_api/product/product.py
@@ -1,7 +1,7 @@
 from ninja import *
 from typing import *
-
 from django.shortcuts import get_object_or_404
+
 from product.models import *
 from webmart_api.product.schema import *
 from webmart_api.utils import *
@@ -14,13 +14,15 @@ product_router = Router()
 def get_products(request, queries: Query[ProductsFilterSchema]):
     return 200, Product.objects.filter(**queries.dict())
 
-@product_router.get('details/{product_id}', response=FullProductOut)
+@product_router.get('get/{product_id}', response=FullProductOut)
 def get_product_details(request, product_id: int):
     return get_object_or_404(Product, id=product_id)
 
 @product_router.post('create', response=ProductOut)
 def create_product(request, payload: ProductCreate=Form()):
-    new_product = Product.objects.create(**payload.dict())
+    data = payload.dict().copy()
+    data.pop("category", None)
+    new_product = Product.objects.create(**data)
     return new_product
 
 @product_router.delete('delete/{product_id}')
