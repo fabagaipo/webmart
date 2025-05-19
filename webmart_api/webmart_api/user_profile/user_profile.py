f45f4ee786639e0e10c17409a1d6630d18e5ab1a
Ar-jay Dayanan <acdayanan.bposeats@gmail.com>
2025-05-20 00:46:45 +0800

    Setup initial router for user_profile

---
+++ webmart_api/webmart_api/user_profile/user_profile.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from django.db import transaction
+from ninja import *
+
+from webmart_api.user_profile.schema import *
+
+
+user_router = Router()
+
+@user_router.post('sign-up')
+def get_users(request, payload: UserCreate=Form()):
+    with transaction.atomic():
+        pass
