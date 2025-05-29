cf1f98a0e01321fff35ec0bc59c9d5d9ea82826c
Ar-jay Dayanan <acdayanan@gmail.com>
2025-05-30 01:07:49 +0800

    Add user api and schema

--- webmart_api/webmart_api/user_profile/schema.py
+++ webmart_api/webmart_api/user_profile/schema.py
@@ -1,26 +1,49 @@
-from ninja import ModelSchema, Schema
+from ninja import *
+from django.db import models
+from typing import *
 from user_profile.models import *
 from django.contrib.auth.models import User
-
+from pydantic import *
 from webmart_api.utils import generate_schema
 
-#class BaseUserSchema(ModelSchema):
-#    pass
-#class BaseUserProfileSchema(ModelSchema):
-#    class Meta:
-#        model = UserProfile
-#        exclude = ["id"]
+class BaseUserSchema(ModelSchema):
+    class Meta:
+        model = User
+        fields = ["first_name", "email", "last_name"]
+    
+    @computed_field
+    def full_name(self) -> str:
+        return f'{self.first_name} {self.last_name}'
+    
+    @staticmethod
+    def resolve_test(obj):
+        return 'test'
 
 class BaseAddressSchema(ModelSchema):
     class Meta:
         model = Address
         fields = "__all__"
-        exclude = ["id", "user_profile"]
-        fields_optional = ["details"]
+        fields_optional = ["details", "id"]
+        exclude = ["user_profile"]
 
+class BaseUserProfileSchema(ModelSchema):
+    address: Optional[list[BaseAddressSchema]] = Field(
+        default = [],
+        alias = "address_list"
+    )
+    user: BaseUserSchema
+    class Meta:
+        model = UserProfile
+        fields = "__all__"
 
 class UserCreate(Schema):
     first_name: str
     email: str
     last_name: str
+    password: str
     address: BaseAddressSchema
+
+class UserSignInForm(Schema):
+    email: str
+    username: str = None
+    password: str
