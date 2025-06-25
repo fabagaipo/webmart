c574d00d6d4dddc730ba42d8358474c861b12e5b
Ar-jay Dayanan <acdayanan@gmail.com>
2025-06-26 01:00:33 +0800

    Modify a few fields of the Address model

--- webmart_api/src/user_profile/models/user_profile.py
+++ webmart_api/src/user_profile/models/user_profile.py
@@ -13,8 +13,9 @@ class Address(models.Model):
     user_profile = models.ForeignKey(
         UserProfile, related_name="address_list", on_delete=models.CASCADE
     )
-    zip_code = models.CharField(max_length=5)
-    city = models.CharField(max_length=50)
-    province = models.CharField(max_length=50)
-    phone_number = models.CharField(max_length=11)
-    details = models.CharField(max_length=50)
+    region_code = models.CharField(max_length=10)
+    province_code = models.CharField(max_length=10)
+    city_code = models.CharField(max_length=10)
+    barangay_code = models.CharField(max_length=10)
+    phone_number = models.CharField(max_length=13)
+    details = models.CharField(max_length=50, blank=True, null=True)
