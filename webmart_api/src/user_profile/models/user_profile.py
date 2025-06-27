02e71d6d210fd3a8252f8c2163921dd4bac8f08a
Ar-jay Dayanan <acdayanan@gmail.com>
2025-06-28 03:22:34 +0800

    Update UserProfile model

--- webmart_api/src/user_profile/models/user_profile.py
+++ webmart_api/src/user_profile/models/user_profile.py
@@ -13,9 +13,10 @@ class Address(models.Model):
     user_profile = models.ForeignKey(
         UserProfile, related_name="address_list", on_delete=models.CASCADE
     )
-    region_code = models.CharField(max_length=10)
-    province_code = models.CharField(max_length=10)
-    city_code = models.CharField(max_length=10)
-    barangay_code = models.CharField(max_length=10)
-    phone_number = models.CharField(max_length=13)
+    region_code = models.CharField(max_length=10, null=True, blank=True)
+    province_code = models.CharField(max_length=10, null=True, blank=True)
+    city_code = models.CharField(max_length=10, null=True, blank=True)
+    barangay_code = models.CharField(max_length=10, null=True, blank=True)
+    phone_number = models.CharField(max_length=13, null=True, blank=True)
+    street = models.CharField(max_length=25, null=True, blank=True)
     details = models.CharField(max_length=50, blank=True, null=True)
