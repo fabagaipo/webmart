475d8766992d7f8542c3783f0fd91fd85664fc17
Ar-jay Dayanan <acdayanan@gmail.com>
2025-06-28 03:26:17 +0800

    Additional vite directory alias

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
             assets: path.resolve(__dirname, './src/assets'),
             context: path.resolve(__dirname, './src/context'),
             webmart: path.resolve(__dirname, './src/webmart-api'),
+            'custom-hooks': path.resolve(__dirname, './src/hooks')
         },
     },
 });
