6fdccdf9f739f0340ec331f357f50aa162ea4d5d
Ar-jay Dayanan <acdayanan.bposeats@gmail.com>
2025-06-28 18:33:51 +0800

    Fix wrong bash scripts

--- additional_setup.sh
+++ additional_setup.sh
@@ -5,9 +5,12 @@
 set -e
 echo "Setting up virtual environment"
 python3 -m venv .venv
+echo "Activating virtual environment"
+source .venv/bin/activate
 echo "Installing pre-commit package manager"
 pip install pre-commit
 echo "Installing pre-commit hooks from pre-commit-config.yaml"
 pre-commit install
-source .venv/bin/deactivate
+echo "Deactivating virtual environment"
+source deactivate
 echo "Done"
