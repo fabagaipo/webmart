030ae507abde5eea239a26f0d86ee88459c13837
Ar-jay Dayanan <acdayanan.bposeats@gmail.com>
2025-07-04 10:06:59 +0800

    Modify UserContext functions to align with the changes of the api client

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -9,9 +9,11 @@ export const UserProvider = ({ children }) => {
 
     const getUser = useCallback(async () => {
         const refreshAccessToken = () => {
-            return WebMartApi('user/refresh-access', 'GET')
-            .then(async (res) => {
-                localStorage.setItem("access_token", res?.access_token)
+            return WebMartApi({
+                endpoint: 'user/refresh-access', method: 'GET'
+            })
+            .then(async (response) => {
+                localStorage.setItem("access_token", response?.access_token)
                 return true
             }).catch(() => {
                 return false
@@ -19,9 +21,13 @@ export const UserProvider = ({ children }) => {
         }
 
         try {
-            const response = await WebMartApi('user/me', 'GET')
-            const { id, avatar_url, address, user } = response.data;
-            setUser(prev => ({...prev, id, avatar_url, address, ...user}));
+            const response = await WebMartApi({
+                endpoint: 'user/me', method: 'GET'
+            })
+            const { id, avatar_data,
+                avatar_url, address, full_name, user } = response;
+            setUser(prev => ({...prev, id, avatar_url, avatar_data, full_name,
+                address, ...user}));
         } catch (error) {
             if (error.code === "access_token_expired") {
                 const refreshed = refreshAccessToken()
@@ -36,41 +42,63 @@ export const UserProvider = ({ children }) => {
     }, [navigate]);
 
     const performSignup = (payload) => {
-        return WebMartApi('user/sign-up', 'POST', payload).then((res) => {
-            const { id, avatar_url, address, user } = res.data;
-            setUser(prev => ({...prev, id, avatar_url, address, ...user}));
-            const { tokens } = res;
+        return WebMartApi({
+            endpoint: 'user/sign-up', method: 'POST', data: payload
+        }).then((response) => {
+            const { id, avatar_data,
+                avatar_url, address, full_name, user } = response;
+            setUser(prev => ({...prev, id, avatar_url, avatar_data, full_name,
+                address, ...user}));
+            const { tokens } = response;
             localStorage.setItem('refresh_token', tokens.refresh_token);
             localStorage.setItem('access_token', tokens.access_token);
-            return res;
+            return response;
         });
     };
 
     const performLogin = (payload) => {
-        return WebMartApi('user/sign-in', 'POST', payload).then((res) => {
-            const { id, avatar_url, address, user } = res.data;
-            setUser(prev => ({...prev, id, avatar_url, address, ...user}));
-            const { tokens } = res;
+        return WebMartApi({
+            endpoint: 'user/sign-in', method: 'POST', data: payload
+        }).then((response) => {
+            const { id, avatar_data,
+                avatar_url, address, full_name, user } = response;
+            setUser(prev => ({...prev, id, avatar_url, avatar_data, full_name,
+                address, ...user}));
+            const { tokens } = response;
             localStorage.setItem('refresh_token', tokens?.refresh_token);
             localStorage.setItem('access_token', tokens?.access_token);
-            return res;
+            return response;
         });
     };
 
     const performLogout = (payload = {}) => {
-        return WebMartApi('user/sign-out', 'POST', payload).then((res) => {
+        return WebMartApi({
+            endpoint: 'user/sign-out', method:'POST', data: payload
+        }).then((response) => {
             setUser({});
             localStorage.clear();
-            return res;
+            return response;
         });
     };
+
+    const updateUserAvatar = (payload = {}) => {
+        return WebMartApi({
+            endpoint: 'user/update-avatar', method: 'PUT',
+            data: payload, type: 'json'
+        }).then((response) => {
+            return response;
+        })
+    }
 
     useEffect(() => {
         getUser();
     }, [getUser]);
 
     return (
-        <UserContext.Provider value={{ user, performLogin, performLogout, performSignup }}>
+        <UserContext.Provider value={{
+                user, performLogin, performLogout, performSignup,
+                updateUserAvatar
+            }}>
             {children}
         </UserContext.Provider>
     );
