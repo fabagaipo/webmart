dd15237b8b33a986fb07ae86dfa1e3b62482d4d3
Ar-jay Dayanan <acdayanan.bposeats@gmail.com>
2025-07-06 23:37:40 +0800

    Adjust profile model and address model to set it up to be more reusable

--- webmart_api/src/user_profile/models/user_profile.py
+++ webmart_api/src/user_profile/models/user_profile.py
@@ -7,20 +7,12 @@ class UserProfile(models.Model):
         User, related_name="user_profile", on_delete=models.CASCADE
     )
     avatar_data = models.JSONField(blank=True, null=True)
-
-    @property
-    def avatar_url(self):
-        return self.avatar_data.get("url") if self.avatar_data else None
-
-    @property
-    def full_name(self):
-        return self.user.get_full_name()
+    addresses = models.ManyToManyField(
+        "user_profile.Address", blank=True, related_name="profiles"
+    )
 
 
 class Address(models.Model):
-    user_profile = models.ForeignKey(
-        UserProfile, related_name="address_list", on_delete=models.CASCADE
-    )
     region_code = models.CharField(max_length=10, null=True, blank=True)
     province_code = models.CharField(max_length=10, null=True, blank=True)
     city_code = models.CharField(max_length=10, null=True, blank=True)
