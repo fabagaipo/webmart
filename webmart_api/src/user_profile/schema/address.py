392856d570e0fef50e90780fc7caeb93616b7d40
Ar-jay Dayanan <acdayanan.bposeats@gmail.com>
2025-07-06 23:35:48 +0800

    Adjustments to user_profile and address schema based on model changes

--- webmart_api/src/user_profile/schema/address.py
+++ webmart_api/src/user_profile/schema/address.py
@@ -1,8 +1,6 @@
 from ninja import (
-    Schema,
     ModelSchema,
 )
-from typing import Optional
 from user_profile.models.user_profile import (
     Address,
 )
@@ -12,12 +10,3 @@ class BaseAddressSchema(ModelSchema):
     class Meta:
         model = Address
         fields = "__all__"
-
-
-class AddressCreate(Schema):
-    region_code: str
-    province_code: str
-    city_code: str
-    barangay_code: str
-    phone_number: str
-    details: Optional[str] = None
--- webmart_api/src/user_profile/schema/user_profile.py
+++ webmart_api/src/user_profile/schema/user_profile.py
@@ -2,37 +2,25 @@ from ninja import Schema, ModelSchema, Field
 from typing import Optional
 
 from user_profile.models.user_profile import UserProfile
-from django.contrib.auth.models import User
 from user_profile.schema.address import (
     BaseAddressSchema,
-    AddressCreate,
 )
 
 
-class BaseUserSchema(ModelSchema):
-    class Meta:
-        model = User
-        fields = ["first_name", "email", "last_name"]
-
-
 class BaseUserProfileSchema(ModelSchema):
-    address: Optional[list[BaseAddressSchema]] = Field(default=[], alias="address_list")
-    user: BaseUserSchema
-    avatar_data: Optional[dict]
-    avatar_url: Optional[str]
-    full_name: str
+    addresses: Optional[list[BaseAddressSchema]]
+    avatar_url: Optional[str] = None
+    email: str = Field(..., alias="user.email")
+    full_name: str = Field(..., alias="user.get_full_name")
+    username: str = Field(..., alias="user.username")
 
     class Meta:
         model = UserProfile
-        fields = "__all__"
+        exclude = ["avatar_data"]
 
-
-class UserCreate(Schema):
-    first_name: str
-    email: str
-    last_name: str
-    password: str
-    address: AddressCreate
+    @staticmethod
+    def resolve_avatar_url(obj):
+        return obj.avatar_data.get("url")
 
 
 class UserSignInForm(Schema):
