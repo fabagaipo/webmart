ac475779822864969d6e666904a67c5b458761a0
Ar-jay Dayanan <acdayanan.bposeats@gmail.com>
2025-08-25 01:16:28 +0800

    Modify sys.path of wsgi to avoid relative absolute importing issues

--- webmart_api/src/_webmart_api/wsgi.py
+++ webmart_api/src/_webmart_api/wsgi.py
@@ -8,8 +8,11 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+
+proj_root = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+sys.path.insert(0, os.path.join(proj_root, "src"))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "_webmart_api.settings")
 
